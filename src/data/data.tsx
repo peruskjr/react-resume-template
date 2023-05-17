6562677f8f81555c2dcc7f596b8578a11c734b32
peruskjr <peruskjr@gmail.com>
2023-05-17 16:45:08 -0400

    Updated testimonials

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -367,7 +367,7 @@ export const testimonial: TestimonialSection = {
     },
     {
       name: 'Someone else',
-      text: 'This guy fucks!',
+      text: 'This guy codes!',
       image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/69.jpg',
     },
   ],
