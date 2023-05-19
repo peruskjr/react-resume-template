52244acc39a39f878bb3f97fe40b5cd63ba8d81e
peruskjr <peruskjr@gmail.com>
2023-05-19 14:26:37 -0400

    Reverted _app.tsx to origin

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,13 @@
-import type {AppProps} from 'next/app';
-import {memo} from 'react';
 import 'tailwindcss/tailwind.css';
 import '../globalStyles.scss';
+
+import type {AppProps} from 'next/app';
+import {memo} from 'react';
 
 const MyApp = memo(({Component, pageProps}: AppProps): JSX.Element => {
   return (
     <>
-      <Component{...pageProps}/>
+      <Component {...pageProps} />
     </>
   );
 });
