cc588b6fad583b7a910f6868fe7046cc8881246a
peruskjr <peruskjr@gmail.com>
2023-05-19 14:44:38 -0400

    Moved memo import to top of chunk

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,8 @@
 import 'tailwindcss/tailwind.css';
 import '../globalStyles.scss';
 
-import type {AppProps} from 'next/app';
 import {memo} from 'react';
+import type {AppProps} from 'next/app';
 import {Analytics} from '@vercel/analytics/react';
 
 
