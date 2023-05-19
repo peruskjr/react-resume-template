58ef516f63e48545396892fee52dbbd471c0b527
peruskjr <peruskjr@gmail.com>
2023-05-19 14:48:11 -0400

    Converted _app.tsx to analytics version

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,13 @@
-import 'tailwindcss/tailwind.css';
-import '../globalStyles.scss';
-
-import {memo} from 'react';
-import type {AppProps} from 'next/app';
-import {Analytics} from '@vercel/analytics/react';
-
-
-const MyApp = memo(({Component, pageProps}: AppProps): JSX.Element => {
+import type { AppProps } from 'next/app';
+import { Analytics } from '@vercel/analytics/react';
+ 
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
       <Analytics />
     </>
   );
-});
-
+}
+ 
 export default MyApp;
