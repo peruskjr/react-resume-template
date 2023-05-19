1a25812d966dc84d94f6dab7a0ea1fb44c6a4ca1
peruskjr <peruskjr@gmail.com>
2023-05-19 14:53:30 -0400

    Reverted to original

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,15 @@
+import 'tailwindcss/tailwind.css';
+import '../globalStyles.scss';
+
 import type {AppProps} from 'next/app';
-import {Analytics} from '@vercel/analytics/react';
- 
-function MyApp({ Component, pageProps }: AppProps) {
+import {memo} from 'react';
+
+const MyApp = memo(({Component, pageProps}: AppProps): JSX.Element => {
   return (
     <>
-      <Component{...pageProps}/>
-      <Analytics />
+      <Component {...pageProps} />
     </>
   );
-}
- 
+});
+
 export default MyApp;
