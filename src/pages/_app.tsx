c4da8652703375775ff09a4d462ecd4b2ddcf3e4
peruskjr <peruskjr@gmail.com>
2023-05-19 13:47:10 -0400

    Reordered imports

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,7 @@
 import 'tailwindcss/tailwind.css';
 import '../globalStyles.scss';
-
-import type {AppProps} from 'next/app';
 import {memo} from 'react';
+import type {AppProps} from 'next/app';
 import {Analytics} from '@vercel/analytics/react';
 
 const MyApp = memo(({Component, pageProps}: AppProps): JSX.Element => {
