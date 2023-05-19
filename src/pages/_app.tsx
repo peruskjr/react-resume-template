8f3936911d09198211acc1aebe025e7c521e4dd2
peruskjr <peruskjr@gmail.com>
2023-05-19 14:32:51 -0400

    Moved memo import up one

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,9 @@ import 'tailwindcss/tailwind.css';
 import '../globalStyles.scss';
 
 import type {AppProps} from 'next/app';
-import {Analytics} from '@vercel/analytics/react';
 import {memo} from 'react';
+import {Analytics} from '@vercel/analytics/react';
+
 
 const MyApp = memo(({Component, pageProps}: AppProps): JSX.Element => {
   return (
