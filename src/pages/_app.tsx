9de65228d10b9da8dea9edd9550740a7e60e4291
peruskjr <peruskjr@gmail.com>
2023-05-19 13:49:02 -0400

    Moved local imports to bottom

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,8 @@
-import 'tailwindcss/tailwind.css';
-import '../globalStyles.scss';
 import {memo} from 'react';
 import type {AppProps} from 'next/app';
 import {Analytics} from '@vercel/analytics/react';
+import 'tailwindcss/tailwind.css';
+import '../globalStyles.scss';
 
 const MyApp = memo(({Component, pageProps}: AppProps): JSX.Element => {
   return (
